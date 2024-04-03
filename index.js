d78e391744dd36efb3b4976a9e7cb4b3e459a806
HIeu <hieule1235@gmail.com>
2024-04-03 18:19:58 +0700

    add bank account

--- index.js
+++ index.js
@@ -112,32 +112,24 @@ const { databaseService } = require("./service");
 const app = express();
 databaseService.connect();
 const acb = new ACB();
-const botToken = "7137690710:AAF5w-cO_y9rtPUYcz9dBdLFdT_fMDcNzBc";
-const chatId = "6449260356";
+
 try {
   sendUnsentTransactionJob.start();
   deleteOldDataJob.start();
 } catch (error) {
   console.log("Error starting cron jobs:", error);
 }
-const bot = new TelegramBot(botToken, { polling: true });
-bot.sendMessage(chatId, "Bot started");
+
 const formatMessage = (data, sodu) => {
   const sodu1 = sodu.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   const transactionNumber = data.transactionNumber;
   const amount = data.amount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   const description = data.description;
-  const account = data.account;
-  const bankName = "ACB";
   const type = data.type === "IN" ? "+" : "-";
-  return `
-          Giao dịch ${type} ${amount} VND
-          Số dư: ${sodu1}
-          Nội dung: ${description}
-          Mã giao dịch: ${transactionNumber}
-          Số tài khoản: ${account} 
-          Ngân hàng: ${bankName}
-      `;
+  return `Giao dịch : ${type} ${amount} VND
+Số dư : ${sodu1} VND
+Nội dung : ${description}
+Mã giao dịch : ${transactionNumber}`;
 };
 
 const loginToACB = async (username, password) => {
@@ -150,11 +142,11 @@ const loginToACB = async (username, password) => {
   }
 };
 
-const fetchTransactions = async (username, password) => {
+const fetchTransactions = async (numberAccount, username, password) => {
   try {
     const accessToken = await loginToACB(username, password);
     const sodu = (await acb.getsodu(accessToken)).data[0].totalBalance;
-    const result = await acb.lsgd("6559411", 10, accessToken);
+    const result = await acb.lsgd(numberAccount, 5, accessToken);
     return [result.data, sodu];
   } catch (error) {
     console.log("Failed to retrieve transaction history:", error);
@@ -162,26 +154,45 @@ const fetchTransactions = async (username, password) => {
   }
 };
 
-const sendMessagesToTelegram = async (transaction, sodu) => {
+const sendMessagesToTelegram = async (bot, listChatId, transaction, sodu) => {
   try {
     const message = formatMessage(transaction, sodu);
-    await bot.sendMessage(chatId, message);
+    const sendMessagePromises = listChatId.map((chatId) => {
+      return bot.sendMessage(chatId, message);
+    });
+
+    // Chờ tất cả các tin nhắn được gửi xong
+    await Promise.all(sendMessagePromises);
+
     console.log(transaction.transactionNumber);
+
+    // Ghi nhận giao dịch đã gửi thành công vào cơ sở dữ liệu
     await databaseService.transactionSent.insertOne({
       transactionNumber: transaction.transactionNumber,
     });
+
     console.log("Message sent successfully");
   } catch (error) {
+    // Ghi nhận giao dịch không thể gửi vào cơ sở dữ liệu
     await databaseService.transactionUnsent.insertOne({ transaction });
+
     console.log("Error sending messages to Telegram:", error);
   }
 };
 
-const handleTransactions = async () => {
+const handleTransactions = async (
+  numberAccount,
+  bot,
+  listChatId,
+  username,
+  password
+) => {
+  // bot.sendMessage(chatId, "handleTransactions started");
   try {
     const [transactions, sodu] = await fetchTransactions(
-      "bin7979",
-      "Lam@3979#"
+      numberAccount,
+      username,
+      password
     );
     for (const transaction of transactions) {
       const result = await databaseService.transactionSent.findOne({
@@ -189,7 +200,7 @@ const handleTransactions = async () => {
       });
       console.log(result);
       if (!result) {
-        await sendMessagesToTelegram(transaction, sodu);
+        await sendMessagesToTelegram(bot, listChatId, transaction, sodu);
       }
     }
   } catch (error) {
@@ -197,11 +208,26 @@ const handleTransactions = async () => {
   }
 };
 
-const startBot = async () => {
+const startBot = async (
+  numberAccount,
+  botToken,
+  listChatId,
+  username,
+  password
+) => {
+  const bot = new TelegramBot(botToken, { polling: true });
+
   while (true) {
     try {
-      await handleTransactions();
-      await new Promise((resolve) => setTimeout(resolve, 4000));
+      await handleTransactions(
+        numberAccount,
+        bot,
+        listChatId,
+        username,
+        password
+      );
+      // bot.stopPolling();
+      await new Promise((resolve) => setTimeout(resolve, 3000));
     } catch (error) {
       console.log("Error starting bot:", error);
     }
@@ -211,7 +237,22 @@ const startBot = async () => {
 const port = process.env.PORT || 4000;
 app.listen(port, async () => {
   console.log(`Server is running on port ${port}`);
-  await startBot();
+  await Promise.all([
+    startBot(
+      "7850491",
+      "6820544893:AAH1D8M58VyK6bVrJc28XQwcoVZTBn-FemI",
+      ["6449260356", "-4191168997"],
+      "meomeo1999",
+      "Thang@1999#"
+    ),
+    startBot(
+      "6559411",
+      "7137690710:AAF5w-cO_y9rtPUYcz9dBdLFdT_fMDcNzBc",
+      ["6449260356", "-4189985331"],
+      "bin7979",
+      "Lam@3979#"
+    ),
+  ]);
 });
 
-exports.sendMessagesToTelegram = sendMessagesToTelegram;
+module.exports = { sendMessagesToTelegram };
