44585857792f9f2f18527dbd3d20efc6651d7cda
Harray Tran <harray.tran@dssolution.jp>
2024-09-07 16:17:59 +0700

    remove account dung

--- index.js
+++ index.js
@@ -1,108 +1,3 @@
-// const express = require("express");
-// const TelegramBot = require("node-telegram-bot-api");
-// const ACB = require("./acb");
-// const { databaseService } = require("./service");
-// const app = express();
-// databaseService.connect();
-// const acb = new ACB();
-// const botToken = "7031591905:AAH10dqivszC6fAsD8zhnREkrxC71g5nEKg";
-// const chatId = "6944161883";
-
-// const bot = new TelegramBot(botToken, { polling: true });
-// bot.sendMessage(chatId, "Bot started");
-// const formatMessage = (data, sodu) => {
-//   const sodu1 = sodu.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-//   const transactionNumber = data.transactionNumber;
-//   const amount = data.amount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-//   const description = data.description;
-//   const account = data.account;
-//   const bankName = "ACB";
-//   const type = data.type === "IN" ? "+" : "-";
-//   return `
-//           Giao dịch ${type} ${amount} VND
-//           Số dư:${sodu1}
-//           Nội dung: ${description}
-//           Mã giao dịch: ${transactionNumber}
-//           Số tài khoản: ${account}
-//           Ngân hàng: ${bankName}
-//       `;
-// };
-
-// const loginToACB = async (username, password) => {
-//   try {
-//     const result = await acb.login(username, password);
-//     return result.accessToken;
-//   } catch (error) {
-//     console.log("Failed to login to ACB:", error);
-//     throw new Error("Failed to login to ACB.");
-//   }
-// };
-
-// const fetchTransactions = async (username, password) => {
-//   try {
-//     const accessToken = await loginToACB(username, password);
-//     const sodu = (await acb.getsodu(accessToken)).data[0].totalBalance;
-//     const result = await acb.lsgd("6559411", 100, accessToken);
-//     return [result.data, sodu];
-//   } catch (error) {
-//     console.log("Failed to retrieve transaction history:", error);
-//     throw new Error("Failed to retrieve transaction history.");
-//   }
-// };
-
-// const sendMessagesToTelegram = async (transaction, sodu) => {
-//   try {
-//     const message = formatMessage(transaction, sodu);
-//     await bot
-//       .sendMessage(chatId, message)
-//       .then(async () => {
-//         console.log(transaction.transactionNumber);
-//         await databaseService.transactionSent.insertOne({
-//           transactionNumber: transaction.transactionNumber,
-//         });
-//         console.log("Message sent successfully");
-//       })
-//       .catch(async (error) => {
-//         await databaseService.transactionUnsent.insertOne({ transaction });
-//       });
-//   } catch (error) {
-//     console.log("Error sending messages to Telegram:", error);
-//   }
-// };
-
-// const handleTransactions = async () => {
-//   try {
-//     const [transactions, sodu] = await fetchTransactions(
-//       "bin7979",
-//       "Lam@3979#"
-//     );
-//     await transactions.forEach(async (transaction) => {
-//       const result = await databaseService.transactionSent.findOne({
-//         transactionNumber: transaction.transactionNumber,
-//       });
-//       console.log(result);
-//       if (!result) {
-//         await sendMessagesToTelegram(transaction, sodu);
-//       }
-//     });
-//   } catch (error) {
-//     console.log("Error handling transactions:", error);
-//   }
-// };
-
-// const startBot = async () => {
-//   try {
-//     setInterval(handleTransactions, 10000); // Default poll interval: 3 seconds
-//   } catch (error) {
-//     console.log("Error starting bot:", error);
-//   }
-// };
-
-// const port = process.env.PORT || 4000;
-// app.listen(port, async () => {
-//   console.log(`Server is running on port ${port}`);
-//   await startBot();
-// });
 const sendUnsentTransactionJob = require("./cronJob").sendUnsentTransactionJob;
 const deleteOldDataJob = require("./cronJob").deleteOldDataJob;
 const express = require("express");
@@ -112,6 +7,7 @@ const { databaseService } = require("./service");
 const app = express();
 databaseService.connect();
 const acb = new ACB();
+let accessToken = null;
 
 try {
   sendUnsentTransactionJob.start();
@@ -143,13 +39,21 @@ const loginToACB = async (username, password) => {
 };
 
 const fetchTransactions = async (numberAccount, username, password) => {
+  let accessToken;
   try {
-    const accessToken = await loginToACB(username, password);
+    accessToken = await loginToACB(username, password);
     const result = await acb.lsgd(numberAccount,5, accessToken);
     return [result.data, accessToken];
-  } catch (error) {
-    console.log("Failed to retrieve transaction history:", error);
-    throw new Error("Failed to retrieve transaction history.");
+  } catch (e) {
+    try{
+      accessToken = await loginToACB(username, password);
+      const result = await acb.lsgd(numberAccount,5, accessToken);
+      return [result.data, accessToken];
+    }
+    catch(e){
+      console.log("Failed to fetch transactions:", e);
+      throw new Error("Failed to fetch transactions.");
+    }
   }
 };
 
@@ -172,9 +76,9 @@ const sendMessagesToTelegram = async (
     console.log(transaction.transactionNumber);
 
     // Ghi nhận giao dịch đã gửi thành công vào cơ sở dữ liệu
-    await databaseService.transactionSent.insertOne({
-      transactionCode: `${transaction.transactionNumber}-${transaction.account}`,
-    });
+    // await databaseService.transactionSent.insertOne({
+    //   transactionCode: `${transaction.transactionNumber}-${transaction.account}`,
+    // });
 
     console.log("Message sent successfully");
   } catch (error) {
@@ -206,6 +110,9 @@ const handleTransactions = async (
       console.log(result);
       if (!result) {
         await sendMessagesToTelegram(bot, listChatId, transaction, accessToken);
+      }
+      else {
+        break;
       }
     }
   } catch (error) {
@@ -263,14 +170,7 @@ app.listen(port, async () => {
       ["6449260356", "-4213276568"],
       "hagiang6868",
       "Giang2003@"
-    ),
-    startBot(
-      "17337651",
-      "7471858101:AAG8er5A7usX3CXgVQ_w9LcdOru3_Yn-ieo",
-      ["6449260356", "-4201625165"],
-      "dungbeo203",
-      "Dung0309@"
-    ),
+    )
   ]);
 });
 
